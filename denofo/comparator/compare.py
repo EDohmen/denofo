d1de57e3327d5c3e9325fc5e2cc82b724a7e214b
Elias Dohmen <e.dohmen@uni-muenster.de>
2025-04-09 15:53:54 +0200

    refactor: improve comparator output; enhance functions and update constants; update tests

--- denofo/comparator/compare.py
+++ denofo/comparator/compare.py
@@ -1,42 +1,47 @@
 from pathlib import Path
 from enum import Enum
 from typing import Any
-from denofo.utils.constants import SUBMODELS, SUBMODEL_FIELDS, FIELDS_OF_LVL1_SUBMODELS
+from denofo.utils.constants import (
+    SUBMODELS,
+    INDENT_LVL_DICT,
+)
 
 
-def _turn_value_to_string(val: Any, sep_num: int = 2) -> str:
+def _turn_value_to_string(val: Any, model_name: str, field_name: str) -> str:
     """
     Turn an element into a string.
 
     :param val: The element to turn into a string.
     :type val: Any
-    :param sep_num: The number of separators.
-    :type sep_num: int
     :return: The element as a string.
     :rtype: str
     """
     val_str = ""
-    tab = "\t"
-    separator = "\n" + sep_num * tab
+    leading_tab_num = max(
+        INDENT_LVL_DICT.get(field_name, 0), INDENT_LVL_DICT.get(model_name, 0)
+    )
 
     if isinstance(val, dict):
-        val_str += separator.join(
+        val_str = "\n".join(
             [
-                f"{k}: {separator + tab if isinstance(v, dict) else ''}"
-                f"{separator + tab if isinstance(v, list) and len(v) > 1 else ''}"
-                f"{_turn_value_to_string(v, sep_num=sep_num + 1)}"
+                (
+                    f"{(leading_tab_num + 1) * '\t'}{k}:\n"
+                    f"{_turn_value_to_string(v, new_model, new_field)}"
+                )
                 for k, v in val.items()
                 if v is not None
+                and (new_model := k if k in SUBMODELS else model_name)
+                and (new_field := k if k not in SUBMODELS else field_name)
             ]
         )
     elif isinstance(val, (list, tuple, set)):
-        val_str += separator.join(
-            [_turn_value_to_string(e, sep_num=sep_num + 1) for e in val]
+        val_str = "\n".join(
+            [_turn_value_to_string(e, model_name, field_name) for e in val]
         )
     elif isinstance(val, Enum):
-        val_str += f"{val.value}"
+        val_str = f"{(leading_tab_num + 1) * '\t'}{val.value}"
     else:
-        val_str += f"{val}"
+        val_str = f"{(leading_tab_num + 1) * '\t'}{val}"
 
     return val_str
 
@@ -57,14 +62,13 @@ def _get_output_string(
     last_comparison_type = ""
     output_string = ""
     tab = "\t"
-    sep_num = 1
     passed_models = set()
 
     if mode == "similarities":
-        output_string += f"Identical values between {name1} and {name2}:\n"
+        output_string += f"Identical values between {name1} and {name2}:\n\n"
         compare_lst = [elem for elem in comparison if elem[0] == "same"]
     elif mode == "differences":
-        output_string += f"Differences between {name1} and {name2}:\n"
+        output_string += f"Differences between {name1} and {name2}:\n\n"
         compare_lst = [elem for elem in comparison if elem[0] != "same"]
 
     for elem in compare_lst:
@@ -75,62 +79,53 @@ def _get_output_string(
         val_lst = elem[3:]
 
         if model != last_model:
-            if model not in passed_models:
-                if model in SUBMODELS:
-                    if SUBMODEL_FIELDS[model] in FIELDS_OF_LVL1_SUBMODELS:
-                        sep_num = 1
-                    output_string += f"\n{sep_num * tab}{SUBMODEL_FIELDS[model]}:\n"
-                    sep_num += 1
-                else:
-                    output_string += f"\n{model}:\n"
-                    sep_num = 1
+            if model not in passed_models and model in SUBMODELS:
+                output_string += f"{INDENT_LVL_DICT[model] * '\t'}{model}:\n"
+                passed_models.add(model)
 
             last_model = model
             last_field = ""
             last_comparison_type = ""
 
         if field != last_field:
-            if field in FIELDS_OF_LVL1_SUBMODELS:
-                sep_num = 1
-            output_string += f"\n{sep_num * tab}{field}:\n"
+            output_string += f"{INDENT_LVL_DICT[field] * tab}{field}:\n"
             last_field = field
             last_comparison_type = ""
         if comparison_type != last_comparison_type:
             prefix_string = True
             last_comparison_type = comparison_type
 
-        vals = _turn_value_to_string(val_lst, sep_num=sep_num)
-        sep_num += 1
+        leading_tabs = (
+            max(INDENT_LVL_DICT.get(field, 0), INDENT_LVL_DICT.get(model, 0)) + 1
+        ) * tab
+        val_str = _turn_value_to_string(val_lst, model, field)
 
         if mode == "similarities":
             if comparison_type == "same":
-                output_string += f"{sep_num * tab}{vals}\n"
+                output_string += f"{val_str}\n"
         elif comparison_type == "diffval":
             prefix_string = (
-                f"\n{sep_num * tab}differing values in {name1} and {name2}:\n"
+                f"\n{leading_tabs}differing values in {name1} and {name2}:\n"
                 if prefix_string
                 else ""
             )
-            output_string += f"{prefix_string}{sep_num * tab}{vals}\n"
+            output_string += f"{prefix_string}{val_str}\n\n"
         elif comparison_type == "2not1":
             prefix_string = (
-                f"\n{sep_num * tab}values in {name2} but not in {name1}:\n"
+                f"\n{leading_tabs}values in {name2} but not in {name1}:\n"
                 if prefix_string
                 else ""
             )
-            output_string += f"{prefix_string}{sep_num * tab}{vals}\n"
+            output_string += f"{prefix_string}{val_str}\n\n"
         elif comparison_type == "1not2":
             prefix_string = (
-                f"\n{sep_num * tab}values in {name1} but not in {name2}:\n"
+                f"\n{leading_tabs}values in {name1} but not in {name2}:\n"
                 if prefix_string
                 else ""
             )
-            output_string += f"{prefix_string}{sep_num * tab}{vals}\n"
+            output_string += f"{prefix_string}{val_str}\n\n"
 
-        passed_models.add(model)
-        sep_num -= 1
-
-    return output_string
+    return output_string.replace("\n\n\n", "\n\n").strip()
 
 
 def write_comparison(
